724f3ec33e298b96cfe42258772e96dcaa4e6372
Moise Tshiibwabwa <moisetshi@hotmail.com>
2021-05-03 15:21:28 +0200

    Responsive nav

---
+++ style.js
@@ -0,0 +1,7 @@
+[document.getElementsByClassName("navButton")[0]].forEach((item) => {
+item.addEventListener("click", (event) => {
+    document.querySelector(".navigationLinks").classList.toggle("active");
+    console.log("hello")
+});
+});
+
